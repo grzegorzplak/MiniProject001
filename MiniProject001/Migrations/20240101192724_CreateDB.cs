a39b84edabd3559480516228b8b333043c9b3b38
Grzegorz <Grzegorz@LAPTOP-2JK6GIJ7>
2024-01-18 16:10:32 +0100

    Add project files.

---
+++ MiniProject001/Migrations/20240101192724_CreateDB.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MiniProject001.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MiniProject001_Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MiniProject001_Categories", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MiniProject001_Categories");
+        }
+    }
+}
---
+++ MiniProject001/Models/Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
+
+namespace MiniProject001.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Category> MiniProject001_Categories { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=LAPTOP-2JK6GIJ7\\SQLEXPRESS;Database=MiniProjects;Trusted_Connection=True;TrustServerCertificate=True;");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
